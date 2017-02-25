b6ccbcc07c1bdd1db9df71acb9fe021887a3c277
Shipra Srivastava <code.shipra@gmail.com>
2017-02-25 21:07:37 +0530

    adds helloname view

--- view.py
+++ view.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse('Hello')
+
+def helloname(request, name='Stranger'):
+    return HttpResponse('Hello' + " " + name +"!")
